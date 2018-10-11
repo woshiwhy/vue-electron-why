4b8dc6bf7190e22c56bd303ce8fd88462978b540
why <4496415676@qq.com>
2018-10-11 09:21:48 +0800

    买卖历史表格修改

---
+++ my-project/src/main.js
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+import axios from 'axios'
+import App from './App'
+import router from './router'
+import store from './store/store'
+import md5 from 'js-md5';
+import {messageTitle} from "./assets/message";
+import {postObj} from "./assets/axios";
+import {loginAjax} from "./assets/loginaxios";
+import 'element-ui/lib/theme-chalk/index.css';
+import ElementUI from 'element-ui';
+import moment from 'moment'
+import i18n from './i18n/i18n';
+Vue.use(ElementUI);
+Vue.prototype.$messageTitle = messageTitle;
+Vue.prototype.$postAxios = postObj;
+Vue.prototype.$loginAjax = loginAjax;
+Vue.prototype.$ajax = axios;
+Vue.prototype.$md5 = md5;
+Vue.prototype.$moment = moment;
+if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
+Vue.config.productionTip = false
+
+new Vue({
+  components: { App },
+  router,
+  store,
+  i18n,
+  template: '<App/>'
+}).$mount('#app')
---
+++ my-project/src/renderer/i18n/language/index.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Administrator on 2018/10/10.
+ */
+import en from './en';
+import cn from './cn';
+export default {
+    en: en,
+    cn: cn
+}
