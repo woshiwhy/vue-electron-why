a1730b648db7c18b7f351a5c79e9a767de62eaff
zclgxf <502098974@qq.com>
2018-11-01 16:38:37 +0800

    弹出框

--- my-project/src/renderer/components/module/updatedversion.vue
+++ my-project/src/renderer/components/module/updatedversion.vue
@@ -6,20 +6,30 @@
             width="4.21rem"
             class="center-dialog"
             center>
-        <p class="setingBg">
-            <span>{{message}}</span>
-        </p>
+       <div>
+           <div v-for="(item,index) in msgData" :key="index">
+               <p class="list">{{item.message}}</p>
+           </div>
+       </div>
         <span slot="footer" class="dialog-footer">
             <el-button type="primary" @click="confirm()">确 定</el-button>
+            <el-button type="primary" @click="close()">取 消</el-button>
         </span>
     </el-dialog>
 </template>
+<style scoped>
+.list{
+    margin: 5px 10px;
+}
+</style>
 <script>
     export default {
         data(){
             return{
                 title:'更新版本',
-                message:'更新',
+                msgData:[{message:'更新1'},
+                    {message:'更新2'},
+                    {message:'更新3'}],
                 upvShow:true
             }
         },
