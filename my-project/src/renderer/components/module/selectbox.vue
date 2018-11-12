25ec80b787c58748666335e88ef5c22dd430fe4f
why <4496415676@qq.com>
2018-11-12 16:12:11 +0800

    Bug修复

---
+++ my-project/src/renderer/components/module/selectbox.vue
@@ -0,0 +1,81 @@
+<template>
+    <el-select class="order-select" v-model="orderList[name]" @change="changeSelect">
+        <el-option
+                :label="allName"
+                value="">
+        </el-option>
+        <el-option
+                v-for="item in optionList"
+                :key="item[type]"
+                :label="item[labelName]"
+                :value="item[type]">
+        </el-option>
+    </el-select>
+</template>
+<style>
+    .order-select{
+        margin-right: 0.16rem;
+    }
+    .order-select .el-input--suffix .el-input__inner {
+         width: 1.35rem;
+    }
+</style>
+<script>
+    export default {
+        props:['selectOption','type','labelName','allName','name'],
+        data () {
+            return {
+                selectVal:''
+            }
+        },
+        created(){
+            if(this.type=='id'){
+                this.$store.dispatch('selectBazzer',this.selectOption[0])
+            }
+        },
+        computed:{
+            optionList(){
+                    return  this.selectOption
+            },
+            orderList(){
+                return this.$store.state.orderList;
+            },
+        },
+        beforeDestroy(){
+            if(this.type=='id') {
+                this.$store.dispatch('selectBazzer', [])
+            }
+        },
+        methods:{
+            changeSelect(ref){
+                switch (this.type){
+                    case 'val': //交易类型
+                        this.$emit('tradeType',ref);
+                        this.orderList.siteId='';
+                        this.orderList.strategyName='';
+                        this.orderList.symbol='';//选择交易所就清空币对
+                        this.orderList.current=1;
+                        break;
+                    case 'id'://交易所选择
+                        this.activeBazzer(ref);
+                        this.orderList.symbol='';//选择交易所就清空币对
+                        this.orderList.current=1;
+                        break;
+                    case 'uniteSymbol'://币对选择
+                        break
+                }
+            },
+            activeBazzer(data){
+                let bazzerList=this.$store.state.bazzer;
+                for(let v of bazzerList){
+                    if(v.id==data){
+                        this.$store.dispatch('selectBazzer',v);
+                        break;
+                    }
+                }
+
+            }
+        }
+
+    }
+</script>
---
+++ my-project/src/renderer/components/page/personalcenter/ordermanagement.vue
@@ -0,0 +1,145 @@
+<template>
+    <div>
+        <div style="margin-bottom: 0.16rem">
+            <!--交易类型-->
+            <select-Box style="margin-left: 0.32rem" @tradeType="tradeTypeFun" :name="'type'" :allName="'全部订单'" :selectOption="tradeType" :type="'val'" :labelName="'name'"></select-Box>
+            <!--自动交易策略-->
+            <template v-if="cashShow">
+                <!--现货交易交易所选择-->
+                <select-Box  :selectOption="bazzerList" :name="'siteId'" :allName="'全部交易所'"  :type="'id'" :labelName="'name'"></select-Box>
+                <!--币对选择-->
+                <select-Box  :selectOption="currList" :name="'symbol'" :allName="'全部币对'" :type="'name'" :labelName="'uniteSymbol'"></select-Box>
+            </template>
+            <el-input style="width: 2.9rem;" v-if="strategyShow" v-model="orderList.strategyName" placeholder="请输入方案名称"></el-input>
+        </div>
+        <tablelist-box style="width:100%;margin-bottom: 0.16rem" :loadingType="loadingType"  :type="'one'" :tableVal="tableVal" :tableName="tableName"></tablelist-box>
+        <el-pagination
+                style="text-align: center"
+                :page-size="15"
+                layout="prev, pager, next"
+                :current-page="number"
+                :current-page.sync="orderList.current"
+                :total="total">
+        </el-pagination>
+    </div>
+</template>
+
+<script>
+    import TableList from '&/historytable'
+    import selectBox from '&/selectbox'
+    export default {
+        data () {
+            return {
+                number:5,
+                total:0,
+                tradeType:[
+                    {
+                        name:'现货交易',
+                        val:'0'
+                    },
+                    {
+                        name:'自动交易',
+                        val:'1'
+                    },
+                    {
+                        name:'平衡交易',
+                        val:'2'
+                    }
+                ],
+                strategyList:[],
+                tableVal: [],
+                loadingType: false,
+                strategyShow:true,//自动交易策略选择
+                cashShow:true,//现货交易选择；
+                setOBj:{}//节流对象
+            }
+        },
+        components: {
+            'tablelist-box': TableList,
+            'select-Box': selectBox
+        },
+        computed: {
+            tableName () { // 成交历史
+                return [
+                    {name: '订单类型', val: 'type'},
+                    {name: '委托时间', val: 'createTime'},
+                    {name: '方案名称', val: 'remark'},
+                    {name: '交易所', val: 'siteName'},
+                    {name: '币对', val: 'symbol'},
+                    {name: '数量', val: 'amount'},
+                    {name: '单价', val: 'price'},
+                    {name: '交易额', val: 'tradeVolume'},
+                    {name: '状态', val: 'status'},
+                    ]
+            },
+            bazzerList () {
+                return this.$store.state.bazzer
+            },
+            bazzerSelect(){
+                return this.$store.state.sopttrading.selectBazzer;
+            },
+            orderList(){
+                return this.$store.state.orderList;
+            },
+            currList(){
+               if( this.bazzerSelect==undefined){
+                   return []
+               }
+                return this.bazzerSelect.symbolList
+            }
+        },
+        watch:{
+            orderList:{
+                handler(data) {
+                    this.orderPost(this.setOBj)
+                 },
+                immediate: true,
+                deep: true
+            }
+        },
+        methods:{
+            orderPost(obj){
+                clearTimeout(obj.throttle);
+                this.tableVal=[];
+                this.loadingType=true;
+                obj.throttle = setTimeout(() => {
+                    this.$postAxios.orderList(this.orderList).then((res) => {
+                        let dataVal = res.data;
+                        this.loadingType = false;
+                        if (dataVal.code == 200) {
+                            this.total = dataVal.data.total;
+                            this.tableVal = dataVal.data.records;
+                        }
+                    }).catch((err) => {
+                        this.loadingType = false;
+                        this.$messageTitle('网络错误，请稍后重试', 'error')
+                    })
+                },500)
+
+            },
+            handleCurrentChange(page){
+                this.orderList.current=page
+            },
+            tradeTypeFun(data){//交易类型选择
+                let type=data.val;
+                switch (data){
+                    case '0'://现货
+                      this.strategyShow=false;
+                      this.cashShow=true;
+                        break;
+                    case '1'://自动
+                        this.strategyShow=true;
+                        this.cashShow=false;
+                        break;
+                    case '2'://平衡
+                        this.strategyShow=false;
+                        this.cashShow=true;
+                        break;
+                    default://全部
+                        this.strategyShow=true;
+                        this.cashShow=true;
+                }
+            }
+        }
+    }
+</script>
